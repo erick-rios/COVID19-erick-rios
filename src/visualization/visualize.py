d46520acbea76600d1d7cbb805b8ceac3ad307ec
erick-rios <erickrg2700@ciencias.unam.mx>
2023-10-26 13:09:03 -0600

    version 0.2.1

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 from IPython.display import  display
+import os
 
 # --------------------------------------------------------------
 # Load data
@@ -9,92 +10,66 @@ from IPython.display import  display
 
 df = pd.read_pickle("../../data/interim/01_data_processed.pkl")
 
+columnsToPlot = df.columns.drop(["other_disease","sex","death", "set","age","patient_type", "pregnant","clasiffication_final"])
 
-# --------------------------------------------------------------
-# Plot single columns
-# --------------------------------------------------------------
-set_df = df[df["set"]==1]
-plt.plot(set_df["death"])
-# --------------------------------------------------------------
-# Plot all labels to see the relation between age and death
-# --------------------------------------------------------------
-for label in df["set"].unique():
-    subset = df[df["set"] == label]
-    value_counts = subset["death"].value_counts().sort_index()
-    value_counts.plot(kind="bar", label=f"Label {label}")
-    plt.xlabel("Death")
-    plt.ylabel("Frequency")
-    plt.legend()
-    plt.show()
-
-for label in df["sex"].unique():
-    subset = df[df["set"] == label]
-    value_counts = subset["death"].value_counts().sort_index()
-    value_counts.plot(kind="bar", label=f"Label {label}")
-    plt.xlabel("Death")
-    plt.ylabel("Frequency")
-    plt.legend()
-    plt.show()
-# --------------------------------------------------------------
-# Adjust plot settings
-# --------------------------------------------------------------
-
-
-# --------------------------------------------------------------
-# Compare medium vs. heavy sets
-# --------------------------------------------------------------
-
-category_df = df.query("set==3").query("sex == 1").reset_index()
-
-fig, ax = plt.subplots()
-category_df.groupby(["diabetes"])["death"].value_counts().plot(kind="bar", label = "Death sorted by age and sex and diabetes")
-ax.set_xlabel("death")
-ax.set_ylabel("frecuency")
-plt.legend()
-
-# --------------------------------------------------------------
-# Compare participants
-# --------------------------------------------------------------
-category_df_male = df.query("set==4").query("sex == 2").reset_index()
-
-fig, ax = plt.subplots()
-category_df_male.groupby(["diabetes"])["death"].value_counts().plot(kind="bar", label = "Death sorted by age and sex and diabetes")
-ax.set_xlabel("death")
-ax.set_ylabel("frecuency")
-plt.legend()
 
 
 # --------------------------------------------------------------
 # Combine plots in one figure
 # --------------------------------------------------------------
-set = 4
-sex = 1
-male = 2
-columns = ["diabetes"]
-combined_plot_df = df.query(f"set == {set}").query(f"sex == {sex}").reset_index(drop=True)
-combined_plot_df_male = df.query(f"set == {set}").query(f"sex == {male}").reset_index(drop=True)
+sets   = df["set"].unique()
+sexes  = df["sex"].unique()
+ages = {1: '0-20', 2: '20-40',3:'40-60',4: '60+'}
+preconditions ={
+    "diabetes": "Diabetes",
+    "copd": "Chronic Obstructive Pulmonary",
+    "asthma": "Asthma",
+    "inmsupr": "Inmunosuppressed",
+    "hipertension": "Hipertension",
+    "cardiovascular": "Heart or Blood Vessels",
+    "renal_chronic": "Chronic Renal",
+    "obesity": "Obesity",
+    "pneumonia": "Pneumonia",
+    "tobacco": "Tobacco User",
+    
+}
 
-# Combina las columnas de la leyenda en una sola columna llamada 'Legend'
-combined_plot_df['Legend'] = combined_plot_df[columns].astype(str).agg('-'.join, axis=1)
-combined_plot_df_male['Legend'] = combined_plot_df_male[columns].astype(str).agg('-'.join, axis=1)
-
-fig, ax = plt.subplots(nrows=2, sharex=True, figsize=(20, 10))
-
-combined_plot_df.groupby('Legend')["death"].value_counts().unstack().plot(kind="bar", ax=ax[0])
-combined_plot_df_male.groupby('Legend')["death"].value_counts().unstack().plot(kind="bar", ax=ax[1])
-
-ax[0].set_xticklabels([])  # Elimina las etiquetas del eje x en el primer histograma
-ax[1].set_xticklabels([])
-
-ax[0].legend(loc="upper center", bbox_to_anchor=(0.5, 0.9, 0.9, 0.1), ncol=3, fancybox=True, shadow=True)
-ax[1].legend(loc="upper center", bbox_to_anchor=(0.5, 0.9, 0.9, 0.1), ncol=1, fancybox=True, shadow=True)
-
-ax[1].set_xlabel("Death")
-ax[1].set_ylabel("Frequency")
-ax[0].set_ylabel("Frequency")
-
-plt.savefig(f"../../reports/figures/historigram{set}.png")
-plt.show()
+for set in sets:
+    for column in columnsToPlot:
+            
+        fig, ax = plt.subplots(nrows=2, sharex=True, figsize=(20, 10))
+        
+        combined_plot_df = df.query(f"set == {set}").query(f"sex == {sexes[0]}").reset_index(drop=True)
+        combined_plot_df_female = df.query(f"set == {set}").query(f"sex == {sexes[1]}").reset_index(drop=True)
+        
+        combined_plot_df['death'] = combined_plot_df['death'].replace({1: 'Death', 2: 'No death'})
+        combined_plot_df_female['death'] = combined_plot_df['death'].replace({1: 'Death', 2: 'No death'})
+        
+        
+        combined_plot_df.groupby(column)["death"].value_counts().unstack().plot(kind="bar", ax=ax[0])
+        combined_plot_df_female.groupby(column)["death"].value_counts().unstack().plot(kind="bar", ax=ax[1])
+        
+        title = f'Age: {ages[set]}, Sex: Female, Precondition: {preconditions[column]}'
+        titleTwo = f'Age: {ages[set]}, Sex: Male, Precondition: {preconditions[column]}'
+        
+        ax[0].set_xticklabels([f" {preconditions[column]}", f"No  {preconditions[column]}"], rotation = 0)  # Elimina las etiquetas del eje x en el primer histograma
+        ax[1].set_xticklabels([f" {preconditions[column]}", f"No  {preconditions[column]}"], rotation = 0)
+        
+        ax[0].set_title(title)
+        ax[1].set_title(titleTwo)
+        
+        ax[0].legend(loc="upper center", bbox_to_anchor=(0.5, 0.9, 0.9, 0.1), ncol=3, fancybox=True, shadow=True)
+        ax[1].legend(loc="upper center", bbox_to_anchor=(0.5, 0.9, 0.9, 0.1), ncol=1, fancybox=True, shadow=True)    
+        
+        ax[1].set_xlabel(preconditions[column])
+        ax[1].set_ylabel("Frequency")
+        ax[0].set_ylabel("Frequency")
+        
+        if not os.path.exists(f"../../reports/figures/{preconditions[column]}"):
+            os.makedirs(f"../../reports/figures/{preconditions[column]}")
+        
+        plt.savefig(f"../../reports/figures/{preconditions[column]}/historigram_{ages[set]}.png")
+        plt.show()
 
 # --------------------------------------------------------------
 # Loop over all combinations and export for both sensors
