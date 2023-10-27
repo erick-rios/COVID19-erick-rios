9740f9ddbe26fcddc44aa3761d239ad8e9e4aad3
erick-rios <erickrg2700@ciencias.unam.mx>
2023-10-27 15:36:30 -0600

    removed outliers

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,98 +1,96 @@
 import numpy as np
-from numbers import Number
-from scipy.integrate import quad
-from typing import List
-import pandas as pd
+import matplotlib.pyplot as plt
+import scipy.special
+
+def plot_binary_outliers(dataset, col, outlier_col, reset_index):
+    """ Plot outliers in case of a binary outlier score. Here, the col specifies the real data
+    column and outlier_col the columns with a binary value (outlier or not).
+
+    Args:
+        dataset (pd.DataFrame): The dataset
+        col (string): Column that you want to plot
+        outlier_col (string): Outlier column marked with true/false
+        reset_index (bool): whether to reset the index for plotting
+    """
+
+    # Taken from: https://github.com/mhoogen/ML4QS/blob/master/Python3Code/util/VisualizeDataset.py
+
+    dataset = dataset.dropna(axis=0, subset=[col, outlier_col])
+    dataset[outlier_col] = dataset[outlier_col].astype("bool")
+
+    if reset_index:
+        dataset = dataset.reset_index()
+
+    fig, ax = plt.subplots()
+
+    plt.xlabel("samples")
+    plt.ylabel("value")
+
+    # Plot non outliers in default color
+    ax.plot(
+        dataset.index[~dataset[outlier_col]],
+        dataset[col][~dataset[outlier_col]],
+        "+",
+    )
+    # Plot data points that are outliers in red
+    ax.plot(
+        dataset.index[dataset[outlier_col]],
+        dataset[col][dataset[outlier_col]],
+        "r+",
+    )
+
+    plt.legend(
+        ["outlier " + col, "no outlier " + col],
+        loc="upper center",
+        ncol=2,
+        fancybox=True,
+        shadow=True,
+    )
+    plt.show()
 
 
-def get_outliers(data: List[float]) -> List[Number]:
-    N = len(data)
-    x_n = np.mean(data)
-    s_x = np.std(data, ddof=1)
+# Insert Chauvenet's function
 
-    def t(x):
-        return np.abs(x - x_n)/s_x
+def mark_outliers_chauvenet(dataset, col, C=2):
+    """Finds outliers in the specified column of datatable and adds a binary column with
+    the same name extended with '_outlier' that expresses the result per data point.
 
-    def G(x):
-        return 1 / np.sqrt(2 * np.pi) * np.e**(-x**2 / 2)
+    Args:
+        dataset (pd.DataFrame): The dataset
+        col (string): The column you want to apply outlier detection to
+        C (int, optional): Degree of certainty for the identification of outliers given the assumption
+                           of a normal distribution, typically between 1 - 10. Defaults to 2.
 
-    def prob(t):
-        return quad(G, -t, t)[0]
+    Returns:
+        pd.DataFrame: The original dataframe with an extra boolean column
+        indicating whether the value is an outlier or not.
+    """
+    dataset = dataset.copy()
+    # Compute the mean and standard deviation.
+    mean = np.mean(dataset[col])
+    std = np.std(dataset[col])
+    N = len(dataset.index)
+    criterion = 1.0 / (C * N)
+    # Consider the deviation for the data points.
+    deviation = abs(dataset[col] - mean) / std
 
-    def n(x):
-        return N*(1-prob(t(x)))
+    # Express the upper and lower bounds.
+    low = -deviation / np.sqrt(C)
+    high = deviation / np.sqrt(C)
+    
+    prob = []
+    mask = []
 
-    outliers = []
-    for i in range(len(data)):
-        n_i = n(data[i])
-        if n_i < 0.5:
-            outliers.append(data[i])
-
-    return outliers
-
-
-def clean_table(df: pd.DataFrame, value: str) -> pd.DataFrame:
-    val_col_name = value
-
-    # Initialize result DataFrame. Outliers will be removed from this table.
-    clean_df = df.copy()
-
-    # Group by all columns except column of values
-    columns = df.columns.values.tolist()
-    columns.remove(val_col_name)
-    grouped_df = df.groupby(columns)
-
-    # Save outlier rows (index list)
-    to_be_removed = []
-
-    for key, group in grouped_df:
-
-        # Get outliers of the group
-        outliers = get_outliers(group[val_col_name].to_list())
-
-        # Find index for each outlier
-        for outlier in outliers:
-
-            # Initialize query
-            query = f'`{val_col_name}` == {outlier}'
-
-            # Build query adding a condition for each column value
-            for column_i in range(len(columns)):
-
-                # Match value and column
-                col_name = columns[column_i]
-                col_value = key[column_i]
-                col_type = group[col_name].dtypes
-
-                # Add quotes if value is not numeric (string or text)
-                if col_type == 'object':
-                    query += f' and `{col_name}` == "{col_value}"'
-                else:
-                    query += f' and `{col_name}` == {col_value}'
-
-            # Run query and get a single index
-            i = df.query(query).index.values[0]
-
-            # Save index
-            to_be_removed.append(i)
-
-    # Remove outliers looping through the index list in decreasing way
-    # (otherwise the integrity of the indices would be compromised)
-    to_be_removed.sort()
-    i = len(to_be_removed) - 1
-    while i >= 0:
-        clean_df.drop(to_be_removed[i], inplace=True)
-        i -= 1
-
-    clean_df.reset_index(drop=True, inplace=True)
-
-    return clean_df
-
-def add_mean_and_std(df: pd.DataFrame, value: str) -> pd.DataFrame:
-    columns = list(df.columns)
-    columns.remove(value)
-    grouped_df = df.groupby(columns, as_index=False)
-    grouped_df = grouped_df.agg({value: ['mean', 'std']})
-    grouped_df.columns = columns + ['mean', 'std']
-
-    return grouped_df
+    # Pass all rows in the dataset.
+    for index in dataset.index:
+        # Determine the probability of observing the point
+        prob.append(
+            1.0 - 0.5 * (scipy.special.erf(high[index]) - scipy.special.erf(low[index]))
+        )
+        # And mark as an outlier when the probability is below our criterion.
+    
+    for i in range (0,N):
+        mask.append(prob[i] < criterion)
+    
+    dataset[col + "_outlier"] = mask
+    return dataset
--- src/features/remove_outliers2.py
+++ src/features/remove_outliers2.py
@@ -1,101 +1,27 @@
-import math
+
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.stats import norm
-from sklearn.neighbors import LocalOutlierFactor 
-import build_features# pip install scikit-learn
+import build_features
 
 # --------------------------------------------------------------
 # Load data
 # --------------------------------------------------------------
-df = pd.read_pickle("/home/soundskydriver/Documents/COVID19-erick-rios/data/interim/01_data_processed.pkl")
-df.columns
+df = pd.read_pickle("../../data/interim/01_data_processed.pkl")
+df = df.drop(columns=["patient_type","other_disease","clasiffication_final","set"])
+df.head()
 
-# --------------------------------------------------------------
-# Interquartile range (distribution based)
-# --------------------------------------------------------------
-# Agrupa los datos por sexo y muerte y crea los boxplots
-def plot_binary_outliers(dataset, col, outlier_col, reset_index):
-    """ Plot outliers in case of a binary outlier score. Here, the col specifies the real data
-    column and outlier_col the columns with a binary value (outlier or not).
+outlierColumns = [df.columns[1]]
 
-    Args:
-        dataset (pd.DataFrame): The dataset
-        col (string): Column that you want to plot
-        outlier_col (string): Outlier column marked with true/false
-        reset_index (bool): whether to reset the index for plotting
-    """
-
-    # Taken from: https://github.com/mhoogen/ML4QS/blob/master/Python3Code/util/VisualizeDataset.py
-
-    dataset = dataset.dropna(axis=0, subset=[col, outlier_col])
-    dataset[outlier_col] = dataset[outlier_col].astype("bool")
-
-    if reset_index:
-        dataset = dataset.reset_index()
-
-    fig, ax = plt.subplots()
-
-    plt.xlabel("samples")
-    plt.ylabel("value")
-
-    # Plot non outliers in default color
-    ax.plot(
-        dataset.index[~dataset[outlier_col]],
-        dataset[col][~dataset[outlier_col]],
-        "+",
-    )
-    # Plot data points that are outliers in red
-    ax.plot(
-        dataset.index[dataset[outlier_col]],
-        dataset[col][dataset[outlier_col]],
-        "r+",
-    )
-
-    plt.legend(
-        ["outlier " + col, "no outlier " + col],
-        loc="upper center",
-        ncol=2,
-        fancybox=True,
-        shadow=True,
-    )
-    plt.show()
-    
-
-
-# Insert Chauvenet's function
-def chauvenet(data, col, alpha=0.05):
-    data = data.copy()
-    
-    z_scores = abs((data[col] - data[col].mean()) / data[col].std())
-    probabilities = 2 * (1 - norm.cdf(z_scores))
-    is_outlier = probabilities < alpha
-    
-    # Create a new column 'column_outlier' with True/False values for outliers
-    data[f'{col}_outlier'] = is_outlier
-    
-    return data 
-
-
-dataset = build_features.clean_table(df, column)
-plot_binary_outliers(dataset, "age", "age_outlier", reset_index = True)
-
-
-
-col = ['age']
 outliersRemovedDf = df.copy()
 
 for value in df["death"].unique():
-    for column in col:
-        dataset = build_features.clean_table(df[df["death"] == value], column)
-        plot_binary_outliers(dataset, "age", "age_outlier", reset_index = True)
+    for column in outlierColumns:
+        dataset = build_features.mark_outliers_chauvenet(df[df["death"] == value], column)
+        build_features.plot_binary_outliers(dataset, "age", "age_outlier", reset_index = True)
         # Replace values marked as outliers with NaN
         outliersRemovedDf.loc[outliersRemovedDf["death"] == value, column] = dataset[column]
         
         numberOutliers = len(df[df["death"] == value]) - len(dataset[column].dropna())
         print(f"Removed {numberOutliers} outliers from {column} for death={value}")
-
 
 
 
